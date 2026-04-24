f8076a9ce2a2c0b637834efe55bb3f589d2008f4
Kevinmat <kevin_mata98@hotmail.com>
2026-04-24 12:13:53 +0200

    Justert på stier for rapport generering

--- app/scripts/report_generator.js
+++ app/scripts/report_generator.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const outputDir = path.join(__dirname, 'output');
+const outputDir = path.join(__dirname, '../output');
+const templatePath = path.join(__dirname, '../templates/template.html');
+const cssPath = path.join(__dirname, '../templates/style.css');
+const dataPath = path.join(__dirname, '../../output.json'); // Ligger i app/ rot under kjøring
 
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
 try {
-    const data = JSON.parse(fs.readFileSync('output.json', 'utf8'));
-
-    let html = fs.readFileSync('template.html', 'utf8');
+    const data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+    let html = fs.readFileSync(templatePath, 'utf8');
 
     html = html.replace('{{status}}', data.status)
                .replace('{{nodes}}', data.nodes)
@@ -16,9 +18,9 @@ try {
                .replace('{{timestamp}}', new Date().toLocaleString());
 
     fs.writeFileSync(path.join(outputDir, 'index.html'), html);
-    fs.copyFileSync('style.css', path.join(outputDir, 'style.css'));
+    fs.copyFileSync(cssPath, path.join(outputDir, 'style.css'));
 
-    console.log("[NODE] Dashboard er generert med ekstern CSS!");
+    console.log("[NODE] Dashboard generert i app/output/");
 } catch (err) {
     console.error("[NODE] Feil:", err.message);
     process.exit(1);
